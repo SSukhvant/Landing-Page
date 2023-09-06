f715b82df64cbda32af445a01228c9056e361c94
Sukhvant Singh <sukhvantsingh581998@gmail.com>
2023-09-06 20:14:07 +0530

    initial commit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,11 +8,11 @@ const Footer = () => {
     <footer>
       <div className='container-fluid'>
       <ul className='footer-links'>
-      <li><a href='#'>Products</a></li>
-      <li><a href='#'>Solutions</a></li>
-      <li><a href='#'>Resources</a></li>
-      <li><a href='#'>Info</a></li>
-      <li><a href='#'><IoLogoTwitter/></a> <a href='#'><BiLogoLinkedin/></a></li>
+      <li><a href='/#'>Products</a></li>
+      <li><a href='/#'>Solutions</a></li>
+      <li><a href='/#'>Resources</a></li>
+      <li><a href='/#'>Info</a></li>
+      <li><a href='/#'><IoLogoTwitter/></a> <a href='/#'><BiLogoLinkedin/></a></li>
       </ul>
       <div className='d-flex justify-content-center align-items-center mt-5'>
       <img src={logo} alt='logo'/>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,23 +5,23 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
     <div className="container-sm">
-      <a className="navbar-brand" href="#"><img src={logo} alt='logo'/></a>
+      <a className="navbar-brand" href="/#"><img src={logo} alt='logo'/></a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav ms-auto me-5 mb-2 mb-lg-0">
           <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">Home</a>
+            <a className="nav-link active" aria-current="page" href="/#">Home</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Pricing</a>
+            <a className="nav-link" href="/#">Pricing</a>
           </li>
           <li className="nav-item">
-          <a className="nav-link" href="#">Features</a>
+          <a className="nav-link" href="/#">Features</a>
         </li>
         <li className="nav-item">
-        <a className="nav-link" href="#">Contact Us</a>
+        <a className="nav-link" href="/#">Contact Us</a>
       </li>
         </ul>
         <div className="d-flex">
