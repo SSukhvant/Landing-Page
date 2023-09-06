2cb161e5254da62225acbb6c1e885f1eab744a36
Sukhvant Singh <sukhvantsingh581998@gmail.com>
2023-09-06 20:27:49 +0530

    initial commit

--- src/constant/image.js
+++ src/constant/image.js
@@ -1,16 +1,16 @@
-import kb1 from '../assets/kb/kb1.png'
-import kb2 from '../assets/kb/kb2.png'
-import kb3 from '../assets/kb/kb3.png'
-import kb4 from '../assets/kb/kb4.png'
-import kb5 from '../assets/kb/kb5.png'
-import kb6 from '../assets/kb/kb6.png'
-import kb7 from '../assets/kb/kb7.png'
-import kb8 from '../assets/kb/kb8.png'
-import kb9 from '../assets/kb/kb9.png'
-import user from '../assets/kb/user.png'
-import mountains from '../assets/bg/Montanhas.png'
+import kb1 from '../assets/kb/kb1.png';
+import kb2 from '../assets/kb/kb2.png';
+import kb3 from '../assets/kb/kb3.png';
+import kb4 from '../assets/kb/kb4.png';
+import kb5 from '../assets/kb/kb5.png';
+import kb6 from '../assets/kb/kb6.png';
+import kb7 from '../assets/kb/kb7.png';
+import kb8 from '../assets/kb/kb8.png';
+import kb9 from '../assets/kb/kb9.png';
+import user from '../assets/kb/user.png';
+import mountains from '../assets/bg/Montanhas.png';
 
-export default {
+const image = {
     kb1,
     kb2,
     kb3,
@@ -23,3 +23,4 @@ export default {
     user,
     mountains
 }
+ export default image;
