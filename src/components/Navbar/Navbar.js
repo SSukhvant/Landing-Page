8170a3fa379aa8b65f74f7d23c569de6c8157910
Sukhvant Singh <sukhvantsingh581998@gmail.com>
2023-09-08 04:42:25 +0530

    initial commit

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,36 +1,42 @@
-import React, { useState, useEffect } from 'react';
-import logo from '../../assets/logo/logo.svg';
-import './Navbar.scss';
+import React, { useState, useEffect, useRef } from "react";
+import logo from "../../assets/logo/logo.svg";
+import "./Navbar.scss";
 
 const Navbar = () => {
-  const [activeSection, setActiveSection] = useState('home');
-  console.log(activeSection)
+  // const button1Ref = useRef(null);
+  const button2Ref = useRef(null);
+
+  const handleButtonClick = (val) => {
+    setActiveSection(val)
+    button2Ref.current.click();
+  };
+  const [activeSection, setActiveSection] = useState("home");
+  console.log(activeSection);
   useEffect(() => {
     const handleScroll = () => {
       // Determine the active section based on scroll position
       const scrollY = window.scrollY;
       // const contactSection = document.getElementById('contact').offsetTop;
-      const kbSection = document.getElementById('kb').offsetTop;
-      const testSection = document.getElementById('testimonials').offsetTop;
+      const kbSection = document.getElementById("kb").offsetTop;
+      const testSection = document.getElementById("testimonials").offsetTop;
 
-      if (scrollY < kbSection-1) {
-        setActiveSection('home'); // Change to 'home'
+      if (scrollY < kbSection - 1) {
+        setActiveSection("home"); // Change to 'home'
       } else if (scrollY < testSection) {
-        setActiveSection('features'); // Change to 'features'
+        setActiveSection("features"); // Change to 'features'
       } else {
-        setActiveSection('contact'); // Change to 'contact'
+        setActiveSection("contact"); // Change to 'contact'
       }
     };
 
     // Add event listener for scroll
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     // Clean up the event listener on unmount
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
-
 
   return (
     <nav className="navbar sticky-top navbar-expand-lg bg-body-tertiary shadow-sm">
@@ -39,6 +45,7 @@ const Navbar = () => {
           <img src={logo} alt="logo" className="nav-logo" />
         </a>
         <button
+          ref={button2Ref}
           className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
@@ -51,23 +58,53 @@ const Navbar = () => {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto me-5 mb-2 mb-lg-0">
-            <li className={`nav-item ${activeSection === 'home' ? 'active' : ''}`}>
-              <a className="nav-link" href="/#" onClick={() => setActiveSection('home')}>
+            <li
+              className={`nav-item ${activeSection === "home" ? "active" : ""}`}
+            >
+              <a
+                className="nav-link"
+                href="/#"
+                onClick={() => handleButtonClick('home')}
+              >
                 Home
               </a>
             </li>
-            <li className={`nav-item ${activeSection === 'contact' ? 'active' : ''}`}>
-              <a className="nav-link" href="/#contact" onClick={() => setActiveSection('contact')}>
+            <li
+              className={`nav-item ${
+                activeSection === "contact" ? "active" : ""
+              }`}
+            >
+              <a
+                className="nav-link"
+                href="/#contact"
+                onClick={() => handleButtonClick('contact')}
+              >
                 Pricing
               </a>
             </li>
-            <li className={`nav-item ${activeSection === 'features' ? 'active' : ''}`}>
-              <a className="nav-link" href="/#kb" onClick={() => setActiveSection('features')}>
+            <li
+              className={`nav-item ${
+                activeSection === "features" ? "active" : ""
+              }`}
+            >
+              <a
+                className="nav-link"
+                href="/#kb"
+                onClick={() => handleButtonClick('features')}
+              >
                 Features
               </a>
             </li>
-            <li className={`nav-item ${activeSection === 'contact' ? 'active' : ''}`}>
-              <a className="nav-link" href="/#contact" onClick={() => setActiveSection('contact')}>
+            <li
+              className={`nav-item ${
+                activeSection === "contact" ? "active" : ""
+              }`}
+            >
+              <a
+                className="nav-link"
+                href="/#contact"
+                onClick={() => handleButtonClick('contact')}
+              >
                 Contact Us
               </a>
             </li>
